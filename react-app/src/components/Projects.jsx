afc69e31eb42d4ded69594d9a0f5fad987f504f2
William A. <121400607+williamandrews2@users.noreply.github.com>
2025-07-28 13:40:01 -0500

    Commit from work, unfinished card structuring.

--- react-app/src/components/Projects.jsx
+++ react-app/src/components/Projects.jsx
@@ -5,9 +5,17 @@ import pixelStrike from "../assets/FPS-screenshot.png";
 import swiftask from "../assets/swiftask-mockup.png";
 import topicLadder from "../assets/topicladder.webp";
 import weatherly from "../assets/weatherly-mockup.png";
+import { WidthFull } from "@mui/icons-material";
 
 function Projects() {
-  const projects = [];
+  const projects = [
+    {
+      imageSrc: battleship,
+      title: "Battleship",
+      description:
+        "A classic game of Battleship built with vanilla JavaScript, using Webpack, Test-Driven Development (TDD) with Jest, and modular ES6 architecture. Players can place ships, attack opponents, and watch explosions as they battle to sink each other's fleets.",
+    },
+  ];
 
   return (
     <Box component="section" id="projects" sx={{ pt: "60px", pb: "60px" }}>
@@ -17,23 +25,42 @@ function Projects() {
         </Typography>
         <Divider
           variant="middle"
-          sx={{ backgroundColor: "primary.main", height: "4px" }}
+          sx={{
+            backgroundColor: "primary.main",
+            height: "4px",
+            width: "100px",
+            mb: "30px",
+            mx: "auto",
+          }}
         />
 
         <Grid container spacing={3}>
-          <Grid size={{ xs: 12, md: 6 }}>
-            <Card
-              elevation={4}
-              sx={{
-                display: "flex",
-                height: "100%",
-                flexDirection: {
-                  xs: "column",
-                  lg: "row",
-                },
-              }}
-            ></Card>
-          </Grid>
+          {projects.map((project) => (
+            <Grid size={{ xs: 12, md: 6 }} key={project.title}>
+              <Card
+                elevation={4}
+                sx={{
+                  display: "flex",
+                  height: "100%",
+                  flexDirection: {
+                    xs: "column",
+                    lg: "row",
+                    backgroundColor: "background.lighter",
+                  },
+                }}
+              >
+                <img
+                  src={project.imageSrc}
+                  alt={project.title}
+                  style={{ width: "100%" }}
+                />
+
+                <Typography component="h3" variant="h3">
+                  {project.title}
+                </Typography>
+              </Card>
+            </Grid>
+          ))}
         </Grid>
       </Container>
     </Box>
