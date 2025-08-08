1c2d2e56a3cecebaaf91edf338cd91655a4d3b40
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2025-08-07 23:26:24 -0500

    Added scroll animaiton to Contact.jsx.

--- react-app/src/components/Contact.jsx
+++ react-app/src/components/Contact.jsx
@@ -8,6 +8,7 @@ import {
   Container,
 } from "@mui/material";
 import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+import { motion } from "motion/react";
 
 function Contact() {
   const iconStyle = {
@@ -19,6 +20,7 @@ function Contact() {
       color: "primary.contrast", // MUI blue
     },
   };
+  const MotionContainer = motion.create(Container);
 
   return (
     <Box
@@ -32,87 +34,94 @@ function Contact() {
         backgroundColor: "background.lighter",
       }}
     >
-      <Typography variant="h3" component="h3" gutterBottom>
-        Contact
-      </Typography>
-      <Divider
-        variant="middle"
-        sx={{
-          backgroundColor: "primary.main",
-          height: "4px",
-          width: "100px",
-          mb: "30px",
-          mx: "auto",
-        }}
-      />
-
-      <Container>
-        <Card
+      <MotionContainer
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true, amount: 0.2 }}
+        transition={{ duration: 0.6 }}
+      >
+        <Typography variant="h3" component="h3" gutterBottom>
+          Contact
+        </Typography>
+        <Divider
+          variant="middle"
           sx={{
-            backgroundColor: "background.main",
-            color: "text.main",
-            maxWidth: 400,
-            minWidth: "50%",
-            margin: "auto",
-            borderRadius: 3,
-            boxShadow: 4,
-            textAlign: "center",
-            p: 2,
+            backgroundColor: "primary.main",
+            height: "4px",
+            width: "100px",
+            mb: "30px",
+            mx: "auto",
           }}
-        >
-          <CardContent>
-            <Typography variant="body1">
-              Let's connect! Click one of the links below to contact me via
-              LinkedIn or Email, or feel free to checkout my GitHub.
-            </Typography>
+        />
 
-            <Divider
-              variant="middle"
-              sx={{
-                backgroundColor: "text.lighter",
-                height: "1px",
-                width: "90%",
-                mt: "20px",
-                mb: "20px",
-                mx: "auto",
-              }}
-            />
+        <Container>
+          <Card
+            sx={{
+              backgroundColor: "background.main",
+              color: "text.main",
+              maxWidth: 400,
+              minWidth: "50%",
+              margin: "auto",
+              borderRadius: 3,
+              boxShadow: 4,
+              textAlign: "center",
+              p: 2,
+            }}
+          >
+            <CardContent>
+              <Typography variant="body1">
+                Let's connect! Click one of the links below to contact me via
+                LinkedIn or Email, or feel free to checkout my GitHub.
+              </Typography>
 
-            <Box sx={{ display: "flex", gap: 2, justifyContent: "center" }}>
-              <IconButton
-                component="a"
-                href="https://github.com/williamandrews2"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="GitHub"
-                sx={iconStyle}
-              >
-                <FaGithub size={72} />
-              </IconButton>
+              <Divider
+                variant="middle"
+                sx={{
+                  backgroundColor: "text.lighter",
+                  height: "1px",
+                  width: "90%",
+                  mt: "20px",
+                  mb: "20px",
+                  mx: "auto",
+                }}
+              />
 
-              <IconButton
-                component="a"
-                href="https://www.linkedin.com/in/william-cs"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="LinkedIn"
-                sx={iconStyle}
-              >
-                <FaLinkedin size={72} />
-              </IconButton>
+              <Box sx={{ display: "flex", gap: 2, justifyContent: "center" }}>
+                <IconButton
+                  component="a"
+                  href="https://github.com/williamandrews2"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="GitHub"
+                  sx={iconStyle}
+                >
+                  <FaGithub size={72} />
+                </IconButton>
 
-              <IconButton
-                component="a"
-                href="mailto:william@williamandrews.net"
-                aria-label="Email"
-                sx={iconStyle}
-              >
-                <FaEnvelope size={72} />
-              </IconButton>
-            </Box>
-          </CardContent>
-        </Card>
-      </Container>
+                <IconButton
+                  component="a"
+                  href="https://www.linkedin.com/in/william-cs"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="LinkedIn"
+                  sx={iconStyle}
+                >
+                  <FaLinkedin size={72} />
+                </IconButton>
+
+                <IconButton
+                  component="a"
+                  href="mailto:william@williamandrews.net"
+                  aria-label="Email"
+                  sx={iconStyle}
+                >
+                  <FaEnvelope size={72} />
+                </IconButton>
+              </Box>
+            </CardContent>
+          </Card>
+        </Container>
+      </MotionContainer>
     </Box>
   );
 }
